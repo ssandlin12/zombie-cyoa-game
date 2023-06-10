77c4d8f391a8bceba891bde7725f611917d7c8b3
ssandlin12 <ssandlin12@gmail.com>
2023-06-10 15:54:12 +0200

    update

--- src/script.js
+++ src/script.js
@@ -9,25 +9,42 @@ class Game {
     startGame() {
         document.getElementById("new-game").style.display = "none";
         document.getElementById("game-active").style.display = "flex";
+        document.getElementById("game-over").style.display = "none"; // Hide the game-over section.
         this.renderStory(this.currentArea);
     }
 
     renderStory(area) {
         let story = this.story[area];
         document.getElementById('description').innerHTML = story.storyPrompt;
-        for (let i = 1; i <= 4; i++) {
+        document.querySelector('#game-active #preview-image img').src = story.image; // Update the image source
+        for (let i = 1; i <= 3; i++) {
             let button = document.getElementById('button' + i);
             if (story.choices[i-1]) {
                 button.style.display = 'block';
                 button.innerHTML = story.choices[i-1].text;
-                button.onclick = () => this.renderStory(story.choices[i-1].destination);
+                button.onclick = () => {
+                    if(story.choices[i-1].outcome === 'gameOver') {
+                        this.gameOver(story.choices[i-1].destination, this.story[story.choices[i-1].destination].image);
+                    } else {
+                        this.renderStory(story.choices[i-1].destination);
+                    }
+                };
+
             } else {
                 button.style.display = 'none';
             }
         }
+    }
+
+    gameOver(destination, gameOverImage) {
+        document.getElementById("game-active").style.display = "none";
+        document.getElementById("game-over").style.display = "flex";
+        document.getElementById("game-over-text").innerHTML = this.story[destination].storyPrompt; // Update the game over reason with the corresponding story prompt.
+        document.querySelector('#game-over img').src = gameOverImage; // Update the game over image.
     }
 }
 
 const game = new Game(zombiesAtWalmart);
 
 document.getElementById("start-button").addEventListener("click", () => game.startGame());
+document.getElementById("try-again").addEventListener("click", () => game.startGame()); // Add event listener to 'try-again' button.
--- src/zombies.js
+++ src/zombies.js
@@ -2,28 +2,29 @@ const zombiesAtWalmart = {
   opener: {
     openerText:
       "This is a text-based, choose-your-own-adventure game where you try to escape from Walmart. See how long you can survive.",
+    image: "images/opener.png",
   },
   aisle1: {
     storyPrompt:
       "You wake up in a Walmart during the middle of the day. The lights are turned off and the place seems eerily empty. You look around and see a person standing across the aisle, turned around. You think about asking for help but something seems off. What do you do next?",
     choices: [
       {
-        text: "Walk over and ask them for help. They can probably explain what's going on.",
-        destination: "aisle1-zombie",
+        text: "1. Walk over and ask them for help. They can probably explain what's going on.",
+        destination: "aisle1zombie",
+        outcome: "continue",
       },
       {
-        text: "Look around first for a weapon in case something weird is happening",
-        destination: "aisle1-weapon",
-      },
-      {
-        text: "Slowly walk backwards and go to the next aisle to keep looking",
+        text: "2. Slowly walk backwards and go to the next aisle to keep looking",
         destination: "aisle2",
+        outcome: "continue",
       },
       {
-        text: "Climb inside the nearby fridge and wait for them to leave.",
-        destination: "aisle1-gameOver",
+        text: "3. Climb inside the nearby fridge and wait for them to leave.",
+        destination: "aisle1gameOver",
+        outcome: "gameOver",
       },
     ],
+    image: "images/aisle1.png",
   },
 
   aisle2: {
@@ -31,132 +32,209 @@ const zombiesAtWalmart = {
       "You enter the next aisle and find shelves filled with canned food. It seems like a good place to stock up on supplies. As you start grabbing cans, you hear a faint noise behind you. What do you do?",
     choices: [
       {
-        text: "Quickly grab as much food as you can and rush to the next aisle.",
+        text: "1. Quickly grab as much food as you can and rush to the next aisle.",
         destination: "aisle3",
+        outcome: "continue",
       },
       {
-        text: "Stay hidden and observe the source of the noise.",
-        destination: "aisle2-noise",
+        text: "2. Stay hidden and observe the source of the noise.",
+        destination: "aisle2noise",
+        outcome: "continue",
       },
       {
-        text: "Drop the cans and run back to the previous aisle.",
-        destination: "aisle2-gameOver",
-      },
-      {
-        text: "Use one of the cans as a distraction and throw it to the other end of the aisle.",
-        destination: "aisle2-distraction",
+        text: "3. Drop the cans and run back to the previous aisle.",
+        destination: "aisle2gameOver",
+        outcome: "gameOver",
       },
     ],
+    image: "images/aisle2.png",
   },
   aisle3: {
     storyPrompt:
       "You find yourself in the electronics aisle. Broken TV screens and scattered cables are all around you. Suddenly, you hear a loud crash coming from the far end. What do you do?",
     choices: [
       {
-        text: "Investigate the source of the crash.",
-        destination: "aisle3-crash",
+        text: "1. Investigate the source of the crash.",
+        destination: "aisle3crash",
+        outcome: "continue",
       },
       {
-        text: "Search the shelves for any useful gadgets or tools.",
-        destination: "aisle3-search",
+        text: "2. Search the shelves for any useful gadgets or tools.",
+        destination: "aisle3search",
+        outcome: "continue",
       },
       {
-        text: "Leave the aisle and move on to the next section.",
-        destination: "aisle3-gameOver",
-      },
-      {
-        text: "Hide behind a pile of TVs and observe the area.",
-        destination: "aisle3-hide",
+        text: "3. Leave the aisle and move on to the next section.",
+        destination: "aisle3gameOver",
+        outcome: "gameOver",
       },
     ],
+    image: "images/aisle3.png",
   },
   aisle4: {
     storyPrompt:
       "You enter the clothing aisle and find rows of empty hangers and torn clothes. It seems like someone has been scavenging through this aisle. What do you do?",
     choices: [
       {
-        text: "Look for any clothing items that might be useful for your journey.",
-        destination: "aisle4-clothes",
+        text: "1. Look for any clothing items that might be useful for your journey.",
+        destination: "aisle4clothes",
+        outcome: "continue",
       },
       {
-        text: "Listen for any sounds or signs of movement.",
-        destination: "aisle4-gameOver",
+        text: "2. Listen for any sounds or signs of movement.",
+        destination: "aisle4gameOver",
+        outcome: "gameOver",
       },
       {
-        text: "Proceed to the next aisle, keeping an eye out for any potential threats.",
+        text: "3. Proceed to the next aisle, keeping an eye out for any potential threats.",
         destination: "aisle5",
-      },
-      {
-        text: "Arrange the hangers to create a barricade at one end of the aisle.",
-        destination: "aisle4-barricade",
+        outcome: "continue",
       },
     ],
+    image: "images/aisle4.png",
   },
   aisle5: {
     storyPrompt:
       "You arrive at the toy aisle, filled with action figures, board games, and toy guns. A remote-controlled car is moving by itself, bumping into the shelves. What do you do?",
     choices: [
       {
-        text: "Pick up the toy car and see if it can be useful.",
-        destination: "aisle5-toycar",
+        text: "1. Pick up the toy car and see if it can be useful.",
+        destination: "aisle5toycar",
+        outcome: "continue",
       },
       {
-        text: "Avoid the noise and proceed to the next aisle.",
+        text: "2. Avoid the noise and proceed to the next aisle.",
         destination: "aisle6",
+        outcome: "continue",
       },
       {
-        text: "Search for a weapon among the toys.",
-        destination: "aisle5-weapon",
-      },
-      {
-        text: "Stay put and observe the area before moving.",
-        destination: "aisle5-gameOver",
+        text: "3. Search for a weapon among the toys.",
+        destination: "aisle5gameOver",
+        outcome: "gameOver",
       },
     ],
+    image: "images/aisle5.png",
   },
   aisle6: {
     storyPrompt:
       "The camping gear aisle is next. Tents, sleeping bags, and various equipment are scattered around. Suddenly, you notice a light coming from one of the tents. What do you do?",
     choices: [
       {
-        text: "Approach the tent cautiously.",
-        destination: "aisle6-tent",
+        text: "1. Approach the tent cautiously.",
+        destination: "aisle6tent",
+        outcome: "continue",
       },
       {
-        text: "Grab some camping gear and move on to the next aisle.",
+        text: "2. Grab some camping gear and move on to the next aisle.",
         destination: "aisle7",
+        outcome: "continue",
       },
       {
-        text: "Ignore the tent and look for other useful items in the aisle.",
-        destination: "aisle6-items",
-      },
-      {
-        text: "Turn around and leave the aisle immediately.",
-        destination: "aisle6-gameOver",
+        text: "3. Ignore the tent and look for other useful items in the aisle.",
+        destination: "aisle6gameOver",
+        outcome: "gameOver",
       },
     ],
+    image: "images/aisle6.png",
   },
   aisle7: {
     storyPrompt:
       "The final aisle is full of gardening tools. It’s quiet and deserted. You see a shovel leaning against a shelf. What do you do?",
     choices: [
       {
-        text: "Pick up the shovel. It might be useful.",
-        destination: "aisle7-shovel",
+        text: "1. Pick up the shovel. It might be useful.",
+        destination: "aisle7shovel",
+        outcome: "continue",
       },
       {
-        text: "Proceed cautiously, looking out for any movement.",
-        destination: "aisle7-gameOver",
+        text: "2. Start barricading the aisle using the gardening tools.",
+        destination: "aisle7barricade",
+        outcome: "continue",
       },
       {
-        text: "Start barricading the aisle using the gardening tools.",
-        destination: "aisle7-barricade",
-      },
-      {
-        text: "Leave the aisle and look for an exit.",
+        text: "3. Leave the aisle and look for an exit.",
         destination: "exit",
+        outcome: "gameOver",
       },
     ],
+    image: "images/aisle7.png",
   },
-  
+  aisle1zombie: {
+    storyPrompt:
+      "As you approach the figure, they slowly turn around. Their eyes are vacant, skin pale and splotchy. You realize too late - this is a zombie! It starts moving towards you. What do you do?",
+    choices: [
+      {
+        text: "1. Fight back!",
+        destination: "aisle2",
+        outcome: "continue",
+      },
+      {
+        text: "2. Run back and head to another aisle.",
+        destination: "aisle3",
+        outcome: "continue",
+      },
+      {
+        text: "3. Freeze in fear.",
+        destination: "aisle1gameOver",
+        outcome: "gameOver",
+      },
+    ],
+    image: "images/aisle1zombie.png",
+  },
+  aisle1gameOver: {
+    storyPrompt:
+      "You climb into the fridge and close the door. However, you're unable to get out again and slowly freeze to death. Game over.",
+    image: "images/aisle1gameOver.png",
+  },
+  aisle2noise: {
+    storyPrompt:
+      "The noise grows louder and you see a shadow moving along the wall. Suddenly, a cat jumps off the shelf and runs away. Phew! That was close. But you're still in a deserted Walmart... What's your next move?",
+    choices: [
+      {
+        text: "1. Proceed to the next aisle.",
+        destination: "aisle3",
+        outcome: "continue",
+      },
+      {
+        text: "2. Go back to the previous aisle.",
+        destination: "aisle1",
+        outcome: "continue",
+      },
+      {
+        text: "3. Hide behind the shelves and wait.",
+        destination: "aisle2gameOver",
+        outcome: "gameOver",
+      },
+    ],
+    image: "images/aisle2noise.png",
+  },
+
+  aisle2gameOver: {
+    storyPrompt:
+      "You run back to the previous aisle, tripping over a can of beans in your haste. You fall and hit your head on the ground, losing consciousness. Game over.",
+    image: "images/aisle2gameOver.png",
+  },
+
+  aisle3crash: {
+    storyPrompt:
+      "You approach the source of the crash. A set of shelves has collapsed, revealing a hole in the wall. It looks like a potential way out, but it's dark and you're not sure what's on the other side. What do you do?",
+    choices: [
+      {
+        text: "1. Investigate the hole.",
+        destination: "aisle3exit",
+        outcome: "continue",
+      },
+      {
+        text: "2. Continue down the aisle.",
+        destination: "aisle4",
+        outcome: "continue",
+      },
+      {
+        text: "3. Turn back and choose another aisle.",
+        destination: "aisle2",
+        outcome: "continue",
+      },
+    ],
+    image: "images/aisle3crash.png",
+  },
 };
