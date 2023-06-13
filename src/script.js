65aab15fb9c29db69a2e1c8ba34e2a64cc4abd46
ssandlin12 <ssandlin12@gmail.com>
2023-06-13 09:29:45 +0200

    minor bug remaining

--- src/script.js
+++ src/script.js
@@ -1,50 +1,100 @@
 class Game {
-    constructor(story) {
-        this.story = story;
-        this.currentArea = "aisle1";
-        this.opener = story.opener.openerText;
-        document.getElementById("opener").innerHTML = this.opener;
-    }
+  constructor(story) {
+    this.story = story;
+    this.currentArea = "aisle1";
+    this.timer = null;
+    this.timeRemaining = 0;
+    this.opener = story.opener.openerText;
+    this.gameOverStatus = false;
+    document.getElementById("opener").innerHTML = this.opener;
+  }
 
-    startGame() {
-        document.getElementById("new-game").style.display = "none";
-        document.getElementById("game-active").style.display = "flex";
-        document.getElementById("game-over").style.display = "none"; // Hide the game-over section.
-        this.renderStory(this.currentArea);
-    }
+  startTimer() {
+    this.timeRemaining = 10; // set time to 10 seconds
+    document.getElementById("timer-text").textContent = "You've been spotted! ";
+    document.getElementById("timer-text").style.color = "red";
+    document.getElementById("timer-text").classList.add("timer-alert");
+    document.getElementById("timer-number").style.display = "flex";
+    document.getElementById("timer-number").textContent = this.timeRemaining;
 
-    renderStory(area) {
-        let story = this.story[area];
-        document.getElementById('description').innerHTML = story.storyPrompt;
-        document.querySelector('#game-active #preview-image img').src = story.image; // Update the image source
-        for (let i = 1; i <= 3; i++) {
-            let button = document.getElementById('button' + i);
-            if (story.choices[i-1]) {
-                button.style.display = 'block';
-                button.innerHTML = story.choices[i-1].text;
-                button.onclick = () => {
-                    if(story.choices[i-1].outcome === 'gameOver') {
-                        this.gameOver(story.choices[i-1].destination, this.story[story.choices[i-1].destination].image);
-                    } else {
-                        this.renderStory(story.choices[i-1].destination);
-                    }
-                };
+    this.timer = setInterval(() => {
+      this.timeRemaining--;
+      document.getElementById("timer-number").textContent = this.timeRemaining;
 
+      if (this.timeRemaining === 0 && this.gameOverStatus === false) {
+        clearInterval(this.timer);
+        this.gameOver("genericZombieDeath", this.story.genericZombieDeath.image);
+      }
+      else if (this.timeRemaining === 0 && this.gameOverStatus === true) {
+        clearInterval(this.timer);}
+    }, 1000); // decrement every second
+  }
+
+  stopTimer() {
+    clearInterval(this.timer);
+    document.getElementById("timer-text").textContent = "Not yet noticed";
+    document.getElementById("timer-text").classList.remove("timer-alert");
+    document.getElementById("timer-number").style.display = "none";
+  }
+
+  startGame() {
+    document.getElementById("new-game").style.display = "none";
+    document.getElementById("game-active").style.display = "flex";
+    document.getElementById("game-over").style.display = "none"; // Hide the game-over section.
+    this.renderStory(this.currentArea);
+  }
+
+  renderStory(area) {
+    let story = this.story[area];
+    document.getElementById("description").innerHTML = story.storyPrompt;
+    document.querySelector("#game-active #preview-image img").src = story.image; // Update the image source
+    for (let i = 1; i <= 3; i++) {
+        let button = document.getElementById("button" + i);
+        if (story.choices[i - 1]) {
+          button.style.display = "block";
+          button.innerHTML = story.choices[i - 1].text;
+          button.onclick = () => {
+            if (story.choices[i - 1].outcome === "gameOver") {
+              this.gameOver(
+                story.choices[i - 1].destination,
+                this.story[story.choices[i - 1].destination].image
+              );
+            } else if (story.choices[i - 1].outcome === "continue") {
+              this.currentArea = story.choices[i - 1].destination;
+              this.renderStory(story.choices[i - 1].destination);
+            } else if (story.choices[i - 1].outcome === "alert") {
+              this.startTimer();
+              this.currentArea = story.choices[i - 1].destination;
+              this.renderStory(story.choices[i - 1].destination);
+            } else if (story.choices[i - 1].outcome === "interject") {
+              this.stopTimer();
+              this.currentArea = story.choices[i - 1].destination;
+              this.renderStory(story.choices[i - 1].destination);
             } else {
-                button.style.display = 'none';
+              this.renderStory(story.choices[i - 1].destination);
             }
+          };
+        } else {
+          button.style.display = "none";
         }
+      }
     }
 
-    gameOver(destination, gameOverImage) {
-        document.getElementById("game-active").style.display = "none";
-        document.getElementById("game-over").style.display = "flex";
-        document.getElementById("game-over-text").innerHTML = this.story[destination].storyPrompt; // Update the game over reason with the corresponding story prompt.
-        document.querySelector('#game-over img').src = gameOverImage; // Update the game over image.
-    }
+
+  gameOver(destination, gameOverImage) {
+    document.getElementById("game-active").style.display = "none";
+    document.getElementById("game-over").style.display = "flex";
+    document.getElementById("game-over-text").innerHTML =
+      this.story[destination].storyPrompt; // Update the game over reason with the corresponding story prompt.
+    document.querySelector("#game-over img").src = gameOverImage; // Update the game over image.
+  }
 }
 
-const game = new Game(zombiesAtWalmart);
+const game = new Game(zombiesAtSuperMart);
 
-document.getElementById("start-button").addEventListener("click", () => game.startGame());
-document.getElementById("try-again").addEventListener("click", () => location.reload()); // Add event listener to 'try-again' button.
+document
+  .getElementById("start-button")
+  .addEventListener("click", () => game.startGame());
+document
+  .getElementById("try-again")
+  .addEventListener("click", () => location.reload()); // Add event listener to 'try-again' button.
--- src/zombies.js
+++ src/zombies.js
@@ -1,22 +1,26 @@
-const zombiesAtWalmart = {
+const zombiesAtSuperMart = {
   opener: {
     openerText:
-      "This is a text-based, choose-your-own-adventure game where you try to escape from Walmart. See how long you can survive.",
+      "This is a text-based, choose-your-own-adventure game where you try to escape from a super store. See how long you can survive.",
     image: "images/opener.png",
+  },
+  genericZombieDeath: {
+    storyPrompt: "The zombie caught up to you and bit you. You now join the ranks of the undead. Game over.",
+    image: "images/genericzombieDeath.png",
   },
   aisle1: {
     storyPrompt:
-      "You wake up in a Walmart during the middle of the day. The place seems eerily empty. You look around and see a person standing across the aisle, turned around. You think about asking for help but you're not sure. What do you do next?",
+      "You wake up in a SuperMart during the middle of the day. You don't know how long you were unconscious. As you look around, you see a person standing across the aisle with his back to you. What do you do next?",
     choices: [
       {
-        text: "1. Walk over and ask them for help. They can probably explain what's going on.",
+        text: "1. Walk over and ask for help. They can probably explain what's going on.",
         destination: "aisle1zombie",
         outcome: "alert",
       },
       {
-        text: "2. Throw something over the aisle to create a diversion.",
+        text: "2. Throw a nearby water bottle at them.",
         destination: "aisle1continue",
-        outcome: "continue",
+        outcome: "interject",
       },
       {
         text: "3. Climb inside the nearby fridge and wait for them to leave.",
@@ -28,37 +32,161 @@ const zombiesAtWalmart = {
   },
   aisle1freezer: {
     storyPrompt:
-      "You climb into the fridge and close the door. However, you're unable to get out again and slowly freeze to death. Game over.",
+      "You climb into the fridge and close the door. The door locks behind you and you slowly freeze to death. Game over.",
     image: "images/aisle1freezer.png",
   },
   aisle1zombie: {
     storyPrompt:
-      "As you approach the figure, they slowly turn around. Their eyes are vacant, skin pale and splotchy. You realize too late that this is a zombie. It starts moving towards you. What do you do?",
+      "As you approach the person, they slowly turn around. Their eyes are vacant, skin pale and splotchy. You realize this is a zombie. It starts moving towards you. What do you do?",
     choices: [
       {
-        text: "1. Rush at the zombie and fight it",
-        destination: "aisle2",
-        outcome: "continue",
+        text: "1. Rush towards the zombie and shove it out of the way.",
+        destination: "aisle1zombieDeath",
+        outcome: "gameOver",
       },
       {
-        text: "2. Run back and head to another aisle.",
-        destination: "aisle3",
-        outcome: "continue",
+        text: "2. Try to run around the zombie and get to the next aisle.",
+        destination: "aisle2",
+        outcome: "alert",
       },
       {
         text: "3. Try to negotiate with the zombie.",
-        destination: "aisle1gameOver",
+        destination: "aisle1zombieYell",
         outcome: "gameOver",
       },
     ],
     image: "images/aisle1zombie.png",
   },
   aisle1zombieDeath: {
-    storyPrompt: "You rush at the zombie and try to fight it. However, it's too strong and bites you. You slowly turn into a zombie. Game over.",
-    image: "images/aisle1zombieDeath.png",
+    storyPrompt: "You rush at the zombie and try to shove it out of the way. However, it's faster than you expected and it catches your arm. Game over.",
+    image: "images/aisle1zombieBite.png",
+  },
+  aisle1zombieYell: {
+    storyPrompt: "Your voice gets louder until you're yelling, which attracts every zombie in the store. They all proceed to bite you. Game over.",
+    image: "images/aisle1zombieYell.png",
   },
   aisle1continue: {
-    storyPrompt: "You throw a nearby box of food into the next aisle. Hearing it, the figure perks up and starts walking away. You decide to sneak ahead to the next aisle",
+    storyPrompt: "You throw a water bottle at them. Your throw completely misses, but fortunately, they end up walking in the direction of the noise. The aisle is now clear.",
+    choices: [
+      {
+        text: 'Continue',
+        destination: "aisle2",
+        outcome: "progress",
+      },
+    ],
     image: "images/aisle1continue.png",
+},
+aisle2: {
+  storyPrompt: "You enter the next aisle and see rows of canned food. You quickly grab one in case you need it later. What do you do now?",
+  choices: [
+    {
+      text: "1. Grab some more canned food. You never know when you'll need it.",
+      destination: "aisle2cannedFood",
+      outcome: "alert",
+    },
+    {
+      text: "2. Hide in the aisle and wait for the zombie to pass.",
+      destination: "aisle2hide",
+      outcome: "alert",
+    },
+    {
+      text: "3. Put the can back on the shelf, you don't need it.",
+      destination: "aisle2putCanBack",
+      outcome: "gameOver",
+    },
+  ],
+  image: "images/aisle2.png",
+},
+aisle2cannedFood: {
+  storyPrompt: "You grab a few more cans of food. As you're loading up, you see a zombie staring at you from down the aisle. What do you do?",
+  choices: [
+    {
+      text: "1. Forget the cans. Pull a shelf down to block the aisle.",
+      destination: "aisle2topple",
+      outcome: "gameOver",
+    },
+    {
+      text: "2. Throw a can of beans at the zombie.",
+      destination: "aisle2continue",
+      outcome: "alert",
+    },
+    {
+      text: "3. Run away carrying your cans of food.",
+      destination: "aisle2foodRun",
+      outcome: "gameOver",
+    },
+  ],
+  image: "images/aisle2cansHands.png",
+},
+aisle2hide: {
+  storyPrompt: "You wait and hide for a minute. As you're about to leave, you turn and see a zombie standing a few feet away. What do you do?",
+  choices: [
+    {
+      text: "1. Throw a can of beans at the zombie.",
+      destination: "aisle2continue",
+      outcome: "alert",
+    },
+    {
+      text: "2. Keep hiding and hope the zombie doesn't see you.",
+      destination: "aisle2hideAgain",
+      outcome: "gameOver",
+    },
+    {
+      text: "3. Call for help. Maybe someone in the store is nearby.",
+      destination: "aisle1zombieYell",
+      outcome: "gameOver",
+    },
+  ],
+  image: "images/aisle2hide.png",
+
+},
+aisle2topple: {
+  storyPrompt: "You pull a shelf down to block the aisle. The shelf falls on you and you are crushed to death. Game over.",
+    image: "images/aisle2topple.png",
+},
+aisle2putCanBack: {
+  storyPrompt: "You start to put the can back but it accidentally falls and hits the ground. The noise attracts zombies from every direction. Game over.",
+    image: "images/aisle2cans.png",
+},
+aisle2foodRun: {
+  storyPrompt: "You run away with your cans of food. However, you trip and fall. Before you can get up, a zombie has reached you. Game over.",
+    image: "images/aisle2cans.png",
+},
+aisle2hideAgain: {
+  storyPrompt: "You try to hide again, but other zombies have now closed in and you have nowhere to run. Game over.",
+    image: "images/aisle2hideAgain.png",
+},
+aisle2continue: {
+  storyPrompt: "The zombie falls backwards and you run past it as other zombies appear. You continue down the aisle, but it's still chasing you as you enter the electronics section.",
+  choices: [
+    {
+      text: 'Continue',
+      destination: "aisle3",
+      outcome: "progress",
+    },
+  ],
+  image: "images/aisle2throw.png",
+},
+aisle3: {
+  storyPrompt: "You wait and hide for a minute. As you're about to leave, you turn and see a zombie standing a few feet away. What do you do?",
+  choices: [
+    {
+      text: "1. Throw a can of beans at the zombie.",
+      destination: "aisle2continue",
+      outcome: "alert",
+    },
+    {
+      text: "2. Keep hiding and hope the zombie doesn't see you.",
+      destination: "aisle2hideAgain",
+      outcome: "gameOver",
+    },
+    {
+      text: "3. Call for help. Maybe someone in the store is nearby.",
+      destination: "aisle1zombieYell",
+      outcome: "gameOver",
+    },
+  ],
+  image: "images/aisle2hide.png",
+
 },
 };
